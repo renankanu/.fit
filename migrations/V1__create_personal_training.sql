289ba488ddd31b200bf190d226f68d00484d47d0
renankanu <renankanu@gmail.com>
2022-04-02 15:55:25 -0300

    refactor: remove datatable cref_type

--- migrations/V1__create_personal_training.sql
+++ migrations/V1__create_personal_training.sql
@@ -3,17 +3,11 @@ CREATE TABLE IF NOT EXISTS `kn_fit_database`.`personal_training` (
   `full_name` VARCHAR(70) NOT NULL,
   `email` VARCHAR(50) NOT NULL,
   `password` CHAR(64) NOT NULL,
-  `cref_type_id` INT UNSIGNED NOT NULL,
+  `cref_type` ENUM('CREF9/PR') NOT NULL,
   `cref_number` TINYINT NOT NULL,
   `create_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
   `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `fk_personal_training_cref_type_idx` (`cref_type_id` ASC) VISIBLE,
-  CONSTRAINT `fk_personal_training_cref_type`
-    FOREIGN KEY (`cref_type_id`)
-    REFERENCES `kn_fit_database`.`cref_type` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
